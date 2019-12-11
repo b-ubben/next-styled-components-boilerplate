5ef75cc81fb84e8c15ecc10cf616705999d41313
Brandon Ubben <brandon@ubben.co>
2019-12-11 09:49:06 -0800

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const RedHeading = styled.h1`
+  color: red;
+`;
+
+const App = () => <RedHeading>Hello World</RedHeading>;
+
+export default App;
