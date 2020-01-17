db9f57bfd5e39cc7fa5241c70131302cf554afda
Brandon Ubben <brandon@ubben.co>
2020-01-17 08:39:37 -0800

    bumped next version and cleaned up index

--- pages/index.js
+++ pages/index.js
@@ -7,20 +7,13 @@ const H1 = styled.h1`
   max-width: 36rem;
 `;
 
-const Figure = styled.figure`
-  img {
-    margin: 1rem auto;
-    max-width: 150px;
-  }
-
-  figcaption {
-    background: #fafafa;
-    border: 1px solid rgba(0, 0, 0, 0.2);
-    border-radius: 4px;
-    box-shadow: 1px 1px 2px 1px rgba(0, 0, 0, 0.1);
-    color: #3a3a3a;
-    padding: 1rem 2rem;
-  }
+const Card = styled.div`
+  background: #fafafa;
+  color: #010101;
+  border: 1px solid rgba(0, 0, 0, 0.25);
+  border-radius: 4px;
+  box-shadow: 1px 2px 1px rgba(0, 0, 0, 0.11);
+  padding: 1rem;
 `;
 
 export default () => {
@@ -35,18 +28,14 @@ export default () => {
       <Wrapper>
         <H1>Next.js + Styled Components Boilerplate</H1>
 
-        <Figure>
-          <img src="https://www.styled-components.com/static/logo.png" alt="styled-components" />
-
-          <figcaption>
-            Powerful CSS-in-JS solution, beloved by many!
-            <br />
-            Check out {''}
-            <a href="https://styled-components.com/" target="_blank" rel="noopener noreferrer">
-              their website
-            </a>
-          </figcaption>
-        </Figure>
+        <Card>
+          Powerful CSS-in-JS solution, beloved by many!
+          <br />
+          Check out&nbsp;
+          <a href="https://styled-components.com/" target="_blank" rel="noopener noreferrer">
+            their website
+          </a>
+        </Card>
       </Wrapper>
     </>
   );
