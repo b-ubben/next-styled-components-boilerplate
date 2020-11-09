1dfeeb30516d535f1b2b0eeace44baeb08087d43
Brandon Ubben <brandon@ubben.co>
2020-11-09 13:36:27 -0800

    upgrade, cleanup

--- pages/_document.js
+++ pages/_document.js
@@ -1,31 +1,30 @@
-import Document from "next/document";
-import { ServerStyleSheet } from "styled-components";
+import Document from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
-class CustomDocument extends Document {
-	static async getInitialProps(ctx) {
-		const sheet = new ServerStyleSheet();
-		const initiRenderPage = ctx.renderPage;
+export default class CustomDocument extends Document {
+  static async getInitialProps(ctx) {
+    const sheet = new ServerStyleSheet();
+    const initiRenderPage = ctx.renderPage;
 
-		try {
-			ctx.renderPage = () => initiRenderPage({
-				enhanceApp: App => props => sheet.collectStyles(<App {...props} />),
-			});
+    try {
+      ctx.renderPage = () =>
+        initiRenderPage({
+          enhanceApp: App => props => sheet.collectStyles(<App {...props} />),
+        });
 
-			const initialProps = await Document.getInitialProps(ctx);
+      const initialProps = await Document.getInitialProps(ctx);
 
-			return {
-				...initialProps,
-				styles: (
-					<>
-						{initialProps.styles}
-						{sheet.getStyleElement()}
-					</>
-				),
-			};
-		} finally {
-			sheet.seal();
-		}
-	}
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
 }
-
-export default CustomDocument;
--- pages/index.js
+++ pages/index.js
@@ -7,16 +7,7 @@ const H1 = styled.h1`
   max-width: 36rem;
 `;
 
-const Card = styled.div`
-  background: #fafafa;
-  color: #010101;
-  border: 1px solid rgba(0, 0, 0, 0.25);
-  border-radius: 4px;
-  box-shadow: 1px 2px 1px rgba(0, 0, 0, 0.11);
-  padding: 1rem;
-`;
-
-export default () => {
+export default function IndexPage() {
   return (
     <>
       <GlobalStyle />
@@ -27,16 +18,7 @@ export default () => {
 
       <Wrapper>
         <H1>Next.js + Styled Components Boilerplate</H1>
-
-        <Card>
-          Powerful CSS-in-JS solution, beloved by many!
-          <br />
-          Check out&nbsp;
-          <a href="https://styled-components.com/" target="_blank" rel="noopener noreferrer">
-            their website
-          </a>
-        </Card>
       </Wrapper>
     </>
   );
-};
+}
